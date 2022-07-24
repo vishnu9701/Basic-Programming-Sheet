4483c3be70b1e548062e15ab0074610fc727dfc5
Vishnu Pandey <92036251+vishnu9701@users.noreply.github.com>
2022-07-24 13:04:47 +0530

    Create Find the ASCII value of a character .cpp

---
+++ String/Find the ASCII value of a character .cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s ="Vishnu";
+    int n=s.length();
+
+    for(int i=0;i<n;i++)
+    {
+        cout<<"ASCII of "<<s[i]<<" is "<<int(s[i])<<endl;
+    } 
+}
