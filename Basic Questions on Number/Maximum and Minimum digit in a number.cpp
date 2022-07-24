704c09ac06e3156f0f18af6da7db7f335961241e
Vishnu Pandey <92036251+vishnu9701@users.noreply.github.com>
2022-07-24 12:54:37 +0530

    Create Maximum and Minimum digit in a number.cpp

---
+++ Basic Questions on Number/Maximum and Minimum digit in a number.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 20826;
+    int min = 0, max = 0;
+    while (n > 0)
+    {
+        int rd = n % 10;
+        if (rd > max)
+            max = rd;
+        if (rd < min)
+            min = rd;
+
+        n = n / 10;
+    }
+    cout << "Minimum Digit is "<<min<<endl;
+    cout<<"Maximum Digit is "<<max<<endl;
+}
