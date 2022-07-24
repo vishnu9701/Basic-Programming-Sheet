33fca8460f3bb0053eaf34e6fe9f1e83108c8648
Vishnu Pandey <92036251+vishnu9701@users.noreply.github.com>
2022-07-24 12:53:12 +0530

    Create Greatest of three numbers.cpp

---
+++ Basic Questions on Number/Greatest of three numbers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a=3,b=60,c=9;
+    if(a>b && a>c)
+    {
+        cout<<a;
+    }
+    else if(b>c)
+    {
+        cout<<b;
+    }
+    else{
+        cout<<c;
+    }
+}
