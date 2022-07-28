220d49510e05ffa7b06b1eb8f85e1cf8cb61ae5f
Vishnu Pandey <92036251+vishnu9701@users.noreply.github.com>
2022-07-28 12:32:03 +0530

    Create Count Number of Nodes in Linked List.cpp

---
+++ Linked List/Count Number of Nodes in Linked List.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+    }
+};
+
+int countNodes(Node* &head)
+{
+    Node* p=head;
+    int count=0;
+    while(p!=NULL)
+    {
+        p=p->next;
+        count++;
+    }
+    return count;
+}
+
+void display(Node *&head)
+{
+    Node *p = head;
+    while (p != NULL)
+    {
+        cout << p->data << " ";
+        p = p->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *one = new Node(3);
+    Node *two = new Node(6);
+    Node *three = new Node(9);
+
+    one->next = two;
+    two->next = three;
+    three->next = NULL;
+
+    Node *head = one;
+    display(head);
+    cout<<"No of Nodes in Linked List is "<<countNodes(head)<<endl;
+}
