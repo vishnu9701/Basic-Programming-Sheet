7a7dd2ec47ea76deaa40b44d9eff7115929d29cf
Vishnu Pandey <92036251+vishnu9701@users.noreply.github.com>
2022-07-28 14:39:45 +0530

    Create Insert at Tail in Doubly Linked List.cpp

---
+++ Linked List/Insert at Tail in Doubly Linked List.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+    Node* prev;
+
+    Node(int x)
+    {
+        data = x;
+        next = NULL;
+        prev=NULL;
+    }
+};
+
+void insert_tail(Node* &head,int val)
+{
+    Node* p=head;
+    Node* temp=new Node(val);
+    while(p->next!=NULL)
+    {
+        p=p->next;
+    }
+    temp->next=p->next;
+    p->next=temp;
+    temp->prev=p;
+    
+}
+
+void Display(Node *&head)
+{
+    Node *p = head;
+    while (p != NULL)
+    {
+        cout << p->data << " ";
+        p = p->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *one = new Node(3);
+    Node *two = new Node(6);
+    Node *three = new Node(9);
+
+    one->next = two;
+    two->next = three;
+    three->next = NULL;
+
+    two->prev=one;
+    three->prev=two;
+
+    Node *head = one;
+    Display(head);
+    insert_tail(head,0);
+    Display(head);
+}
