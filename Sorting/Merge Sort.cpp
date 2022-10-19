9b4a02fb39231432fe94eac83607bb94478bbaab
Vishnu Pandey <92036251+vishnu9701@users.noreply.github.com>
2022-10-19 12:32:52 +0530

    Create Merge Sort.cpp

---
+++ Sorting/Merge Sort.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+void Merge(int arr[], int low, int mid, int high)
+{
+    int temp[high + 1];
+    int i = low;
+    int j = mid + 1;
+    int k = 0;
+
+    while (i <= mid && j <= high)
+    {
+        if (arr[i] <= arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+
+    while (i <= mid)
+    {
+        temp[k++] = arr[i++];
+    }
+    while (j <= high)
+    {
+        temp[k++] = arr[j++];
+    }
+
+     k--;
+
+    while (k >= 0)
+    {
+        arr[k + low] = temp[k--];
+    }
+}
+void MergeSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int mid = (low + high) / 2;
+        MergeSort(arr, low, mid);
+        MergeSort(arr, mid + 1, high);
+        Merge(arr, low, mid, high);
+    }
+}
+
+int main()
+{
+    int arr[] = {3,1,4,2,5};
+    int n = sizeof(arr) / sizeof(int);
+    cout << "Unsorted Array :";
+    print(arr, n);
+    MergeSort(arr, 0, n - 1);
+    cout << "Sorted Array :";
+    print(arr, n);
+}#include <bits/stdc++.h>
+using namespace std;
+
+void print(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+void Merge(int arr[], int low, int mid, int high)
+{
+    int temp[high + 1];
+    int i = low;
+    int j = mid + 1;
+    int k = 0;
+
+    while (i <= mid && j <= high)
+    {
+        if (arr[i] <= arr[j])
+            temp[k++] = arr[i++];
+        else
+            temp[k++] = arr[j++];
+    }
+
+    while (i <= mid)
+    {
+        temp[k++] = arr[i++];
+    }
+    while (j <= high)
+    {
+        temp[k++] = arr[j++];
+    }
+
+     k--;
+
+    while (k >= 0)
+    {
+        arr[k + low] = temp[k--];
+    }
+}
+void MergeSort(int arr[], int low, int high)
+{
+    if (low < high)
+    {
+        int mid = (low + high) / 2;
+        MergeSort(arr, low, mid);
+        MergeSort(arr, mid + 1, high);
+        Merge(arr, low, mid, high);
+    }
+}
+
+int main()
+{
+    int arr[] = {3,1,4,2,5};
+    int n = sizeof(arr) / sizeof(int);
+    cout << "Unsorted Array :";
+    print(arr, n);
+    MergeSort(arr, 0, n - 1);
+    cout << "Sorted Array :";
+    print(arr, n);
+}
