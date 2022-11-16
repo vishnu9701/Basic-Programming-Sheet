6341d1485773e9c353ac26f72ea144a8e71d066d
Vishnu Pandey <92036251+vishnu9701@users.noreply.github.com>
2022-11-16 10:53:55 +0530

    Create Inorder Traversal.cpp

---
+++ Binaray Tree/Inorder Traversal.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void inorder(Node *root)
+{
+    if (root == NULL)
+        return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    inorder(root);
+}
