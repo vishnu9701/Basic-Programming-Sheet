6f1aed2ced4700d766d34a0c51a498da478eab3e
Vishnu Pandey <92036251+vishnu9701@users.noreply.github.com>
2022-11-16 10:52:24 +0530

    Create Preorder Traversal.cpp

---
+++ Binaray Tree/Preorder Traversal.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void preorder(Node *root)
+{
+    if (root == NULL)
+        return;
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    preorder(root);
+}
