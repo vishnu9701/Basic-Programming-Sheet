09a728a779744a4a59519855210ca1a2ca2fdd30
Vishnu Pandey <92036251+vishnu9701@users.noreply.github.com>
2022-11-16 15:52:56 +0530

    Create Level Order Traversal.cpp

---
+++ Binaray Tree/Level Order Traversal.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void Level_Travresal(Node *root)
+{
+    if (root == NULL)
+        return;
+
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *node = q.front();
+        cout << node->data << " ";
+        if (node->left)
+            q.push(node->left);
+        if (node->right)
+            q.push(node->right);
+        q.pop();
+    }
+}
+
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    Level_Travresal(root);
+}
