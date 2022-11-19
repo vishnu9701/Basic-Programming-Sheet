1fc54944346c1fdeacee56095cd334f742b6dd84
Vishnu Pandey <92036251+vishnu9701@users.noreply.github.com>
2022-11-19 11:03:39 +0530

    Update Build Tree from Preorder and Inorder Travseral.cpp

--- Binaray Tree/Build Tree from Preorder and Inorder Travseral.cpp
+++ Binaray Tree/Build Tree from Preorder and Inorder Travseral.cpp
@@ -11,49 +11,74 @@ public:
     Node(int val)
     {
         data = val;
-        left = NULL;
-        right = NULL;
+        left = right = NULL;
     }
 };
 
-int Search(int inorder[], int start, int end, int curr)
+int Search(int inorder[], int start, int end, int root)
 {
     for (int i = start; i <= end; i++)
     {
-        if (inorder[i] == curr)
+        if (inorder[i] == root)
             return i;
     }
     return -1;
 }
-Node *Build_Tree(int Preorder[], int Inorder[], int start, int end)
+
+Node *Build_Tree(int preorder[], int inorder[], int start, int end)
 {
     if (start > end)
         return NULL;
-    static int i = 0;
 
-    int curr = Preorder[i];
-    i++;
-    int pos = Search(Inorder, start, end, curr);
+    static int i = start;
 
-    Node *node = new Node(curr);
+    int root = preorder[i++];
+    int pos = Search(inorder, start, end, root);
+
+    Node *node = new Node(root);
+
     if (start == end)
         return node;
-    node->left = Build_Tree(Preorder, Inorder, start, pos - 1);
-    node->right = Build_Tree(Preorder, Inorder, pos + 1, end);
+
+    node->left = Build_Tree(preorder, inorder, start, pos - 1);
+    node->right = Build_Tree(preorder, inorder, pos + 1, end);
     return node;
 }
-void inord(Node *root)
+
+void Preorder(Node *root)
 {
     if (root == NULL)
         return;
-    inord(root->left);
-    cout << root->data;
-    inord(root->right);
+
+    cout << root->data << " ";
+    Preorder(root->left);
+    Preorder(root->right);
 }
+
 int main()
 {
-    int Preorder[] = {4, 2, 5, 1, 6, 3, 7};
-    int Inorder[] = {1, 2, 4, 5, 3, 6, 7};
-    Node *root = Build_Tree(Preorder, Inorder, 0, 6);
-    inord(root);
+
+    /*
+          1
+        /   \
+      2      3
+     / \    /  \
+    4   5  6   7
+
+
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    */
+
+    int preorder[] = {1, 2, 4, 5, 3, 6, 7};
+    int inorder[] = {4, 2, 5, 1, 6, 3, 7};
+
+    Node *root = Build_Tree(preorder, inorder, 0, 6);
+    Preorder(root);
 }
