76097bccc6761b6a5b3ee852d9e68f61555c6094
Vishnu Pandey <92036251+vishnu9701@users.noreply.github.com>
2022-11-19 11:06:49 +0530

    Update Implementing Binary Tree.cpp

--- Binaray Tree/Implementing Binary Tree.cpp
+++ Binaray Tree/Implementing Binary Tree.cpp
@@ -11,16 +11,26 @@ public:
     Node(int val)
     {
         data = val;
-        left = NULL;
-        right = NULL;
+        left = right = NULL;
     }
 };
 
 int main()
 {
+
+    /*
+          1
+        /   \
+      2      3
+     / \    /  \
+    4   5  6   7
+
+    */
     Node *root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
     root->left->left = new Node(4);
     root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
 }
