1dfe6765af7f216dd823984178dd14a1ef6bfda7
Vishnu Pandey <92036251+vishnu9701@users.noreply.github.com>
2022-11-19 11:06:01 +0530

    Update Level Order Traversal.cpp

--- Binaray Tree/Level Order Traversal.cpp
+++ Binaray Tree/Level Order Traversal.cpp
@@ -11,12 +11,11 @@ public:
     Node(int val)
     {
         data = val;
-        left = NULL;
-        right = NULL;
+        left = right = NULL;
     }
 };
 
-void Level_Travresal(Node *root)
+void Level_Order(Node *root)
 {
     if (root == NULL)
         return;
@@ -27,22 +26,40 @@ void Level_Travresal(Node *root)
     while (!q.empty())
     {
         Node *node = q.front();
-        cout << node->data << " ";
-        if (node->left)
-            q.push(node->left);
-        if (node->right)
-            q.push(node->right);
         q.pop();
+        if (node != NULL)
+        {
+            cout << node->data << " ";
+            if (node->left)
+                q.push(node->left);
+            if (node->right)
+                q.push(node->right);
+        }
+        else if (!q.empty())
+        {
+            q.push(NULL);
+        }
     }
 }
 
 int main()
 {
+
+    /*
+          1
+        /   \
+      2      3
+     / \    /  \
+    4   5  6   7
+
+    */
     Node *root = new Node(1);
     root->left = new Node(2);
     root->right = new Node(3);
     root->left->left = new Node(4);
     root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
 
-    Level_Travresal(root);
+    Level_Order(root);
 }
